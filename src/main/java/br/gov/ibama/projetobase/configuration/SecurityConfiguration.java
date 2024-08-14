f134d23475476ed8ae21b6529f82d5c255aa1be0
susan Braun Rosa <sb2901@gmail.com>
2024-08-14 08:41:32 -0300

    Ajustes diversos

---
+++ src/main/java/br/gov/ibama/projetobase/configuration/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package br.gov.ibama.projetobase.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    public static final String [] NO_AUTH_PATH = {
+            "/pessoa",
+            "/pessoa/",
+            "/pessoa/*",
+            "/pessoa/**"
+
+    };
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+       // configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://URL_FRONT:PORT"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(NO_AUTH_PATH).permitAll();
+                    request.anyRequest().denyAll();
+                } )
+                .build();
+    }
+}
--- src/test/java/br/gov/ibama/projetobase/TestPessoaController.java
+++ src/test/java/br/gov/ibama/projetobase/TestPessoaController.java
@@ -3,7 +3,6 @@ package br.gov.ibama.projetobase;
 import br.gov.ibama.projetobase.controller.PessoaController;
 import br.gov.ibama.projetobase.dto.PessoaDTO;
 import br.gov.ibama.projetobase.service.impl.PessoaServiceImpl;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,14 +11,23 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.io.IOException;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,20 +57,188 @@ public class TestPessoaController {
                 .andExpect(content().json(getListaPessoa()));
     }
 
-
     @Test
     @DisplayName("Teste get/pessoa/id: Retornar a pessoa referente ao id informado")
     public void getPessoaInformada() throws Exception {
 
         mockMvc.perform(get("/pessoa/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().json(getPessoa()));
+                .andExpect(content().json(getPessoaAsJson()));
     }
+
+    @Test
+    @DisplayName("Teste get/pessoa/id: Retorno de id inexistente")
+    public void getPessoaInformadaInexistente() throws Exception {
+
+        mockMvc.perform(get("/pessoa/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    @DisplayName("Teste delete/pessoa: Remove uma pessoa ")
+    public void deleteId() throws Exception {
+        mockMvc.perform(delete("/pessoa/1")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    @DisplayName("Teste delete/pessoa: Remove uma pessoa com com id  inexistente")
+    public void deleteIdInexistente() throws Exception {
+        mockMvc.perform(delete("/pessoa/2")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+
+    @Test
+    @DisplayName("Teste post/pessoa: Insere uma nova pessoa")
+    public void postPessoa() throws Exception {
+
+        mockMvc.perform(
+                        post("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+    /**
+     * Atenção: este é um método de exemplo
+     * Os recursos de checagem não formam implementados
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Teste post/pessoa: Insere uma nova pessoa com id previamente informado")
+    public void postPessoaComId()  throws Exception {
+        mockMvc.perform(
+                        post("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+    /**
+     * Atenção: este é um método de exemplo
+     * Os recursos de checagem de CPF não formam implementados
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Teste post/pessoa: Insere uma nova pessoa com cpf duplicado")
+    public void postPessoaComCpfDuplicado() throws Exception {
+        mockMvc.perform(
+                        post("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+    /**
+     * Atenção: este é um método de exemplo
+     * Os recursos de checagem de CPF não formam implementados
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Teste post/pessoa: Insere uma nova pessoa com cpf inválido")
+    public void postPessoaComCpfInvalido() throws Exception {
+        mockMvc.perform(
+                        post("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+
+    @Test
+    @DisplayName("Teste put/pessoa: Atualiza uma pessoa")
+    public void atualizaPessoa() throws Exception {
+        mockMvc.perform(
+                        put("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+    /**
+     * Atenção: este é um método de exemplo
+     * Os recursos de checagem de CPF não formam implementados
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Teste put/pessoa: Atualiza uma pessoa com com CPF existente")
+    public void xx3() throws Exception {
+        mockMvc.perform(
+                        put("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+    /**
+     * Atenção: este é um método de exemplo
+     * Os recursos de checagem de CPF não formam implementados
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Teste put/pessoa: Atualiza uma nova pessoa com cpf inválido")
+    public void updatePessoaComCpfInvalido() throws Exception {
+        mockMvc.perform(
+                        put("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+    /**
+     * Atenção: este é um método de exemplo
+     * Os recursos de checagem de CPF não formam implementados
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Teste put/pessoa: Atualiza uma pessoa com com id  inexistente")
+    public void xx1() throws Exception {
+        mockMvc.perform(
+                        put("/pessoa")
+                                .with(csrf())
+                                .content(getPessoaAsJson())
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getPessoaAsJson()));
+    }
+
+
 
     @BeforeEach
     public void preparaService(){
         when(service.getAll()).thenReturn(List.of(getPessoaDTO()));
         when(service.get(1l)).thenReturn(getPessoaDTO());
+        when(service.get(2l)).thenReturn(null);
+        when(service.add(any(PessoaDTO.class))).thenReturn(getPessoaDTO());
+        when(service.update(any(PessoaDTO.class))).thenReturn(getPessoaDTO());
     }
     /**
      * @return
@@ -78,7 +254,7 @@ public class TestPessoaController {
      * @return
      * @throws IOException
      */
-    private String getPessoa() throws IOException {
+    private String getPessoaAsJson() throws IOException {
         return pessoaDToJsonConverter
                 .write(getPessoaDTO())
                 .getJson();
